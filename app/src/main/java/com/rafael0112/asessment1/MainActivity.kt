538f85269d37a01fa5c81660597a268b095f7ba2
Rafael Abednego <rafaelabednego123@gmail.com>
2025-04-11 06:08:55 +0700

    Membuat component navigation dan halaman about

--- app/src/main/java/com/rafael0112/asessment1/MainActivity.kt
+++ app/src/main/java/com/rafael0112/asessment1/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.NavGraph
+import com.rafael0112.asessment1.navigation.SetupNavGraph
 import com.rafael0112.asessment1.ui.theme.Asessment1Theme
 import com.rafael0112.asessment1.ui.screen.MainScreen
 
@@ -13,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Asessment1Theme {
-                MainScreen()
+                SetupNavGraph()
             }
         }
     }
---
+++ app/src/main/java/com/rafael0112/asessment1/navigation/NavGraph.kt
@@ -0,0 +1,21 @@
+package com.rafael0112.asessment1.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.rafael0112.asessment1.ui.screen.MainScreen
+
+@Composable
+fun SetupNavGraph(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(route = Screen.Home.route) {
+            MainScreen()
+        }
+
+    }
+}
