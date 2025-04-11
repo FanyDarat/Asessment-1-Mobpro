524f9569326c061dd991a6606c8f28366f542d3f
Rafael Abednego <rafaelabednego123@gmail.com>
2025-04-11 07:13:19 +0700

    Mengedit halaman about

--- app/src/main/java/com/rafael0112/asessment1/ui/screen/AboutScreen.kt
+++ app/src/main/java/com/rafael0112/asessment1/ui/screen/AboutScreen.kt
@@ -1,7 +1,12 @@
 package com.rafael0112.asessment1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -13,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,14 +55,25 @@ fun AboutScreen(navController: NavHostController) {
             )
         }
     ) { innerPadding ->
-//        ScreenContent(Modifier.padding(innerPadding))
-        Text(
-            text = stringResource(id = R.string.copyright),
-            modifier = Modifier.padding(innerPadding).padding(16.dp)
-        )
+        ContentAboutScreen(Modifier.padding(innerPadding))
     }
 }
 
+@Composable
+fun ContentAboutScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.copyright)
+        )
+
+    }
+}
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
